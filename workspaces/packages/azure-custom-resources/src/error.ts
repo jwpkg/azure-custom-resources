a42163b70d29e536de33f8349d9d8a5a4cf6f774
Joost van der Waal <joost@jvdwaal.nl>
2024-07-31 13:33:14 +0200

    feat: Added unrecoverable error type

--- workspaces/packages/azure-custom-resources/src/error.ts
+++ workspaces/packages/azure-custom-resources/src/error.ts
@@ -22,6 +22,15 @@ export class RequestInputError extends CoreError {
   }
 }
 
+/**
+ * Since 5xx errors are retried we throw a 400 to stop the process
+ */
+export class UnrecoverableServerError extends CoreError {
+  constructor(message: string, public errorCode: string = 'UnrecoverableServerError', public errorStatusCode: number = 400) {
+    super(message, errorCode, errorStatusCode);
+  }
+}
+
 export class NotFoundError extends CoreError {
   constructor(message: string, public errorCode: string = 'NotFoundError', public errorStatusCode: number = 404) {
     super(message, errorCode, errorStatusCode);
